e049e48e37ad89ca4551f32ed8b6c6ab3cac8912
Mason Arditi <masonarditi@berkeley.edu>
2025-06-05 13:54:39 -0700

    inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import { NavigationHeader } from "@/components/navigation-header";
 import { Footer } from "@/components/footer";
 
-
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: "Free Stuff for College Builders",
@@ -22,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body className="min-h-screen flex flex-col">
         <NavigationHeader />
         <main className="flex-1">
