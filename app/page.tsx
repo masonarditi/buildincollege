828d7156e231f1b63c91be51b869a8fee000b1a2
Mason Arditi <masonarditi@berkeley.edu>
2025-08-25 13:35:13 -0700

    20k

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
           variants={containerVariants}
         >
           <motion.div variants={itemVariants}>
-            <ReviewComponent count={10000} />
+            <ReviewComponent count={20000} />
           </motion.div>
           
           <motion.div className="flex gap-4 flex-col" variants={itemVariants}>
